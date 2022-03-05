ac40110a6b91dfcc046df1b8fe26083d612bfc57
unknown <anastasovdavid@gmail.com>
2022-03-05 17:42:22 +0100

    implemented camera

---
+++ lib/widgets/image_picker.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  final Function(File pickedImage) imagePickFn;
+  const UserImagePicker({this.imagePickFn, Key key})
+      : super(key: key);
+
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  void _pickImage(bool isCamera) async {
+    final pickedImageFile = await ImagePicker.platform.pickImage(
+      source: isCamera ? ImageSource.camera : ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+    if (pickedImageFile != null) {
+      final pickedImageAsFile = File(pickedImageFile.path);
+      setState(() {
+        _pickedImage = pickedImageAsFile;
+      });
+      widget.imagePickFn(pickedImageAsFile);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton.icon(
+              onPressed: () => _pickImage(true),
+              icon: Icon(Icons.photo_camera),
+              label: Text('Take a picture'),
+            ),
+            FlatButton.icon(
+              onPressed: () => _pickImage(false),
+              icon: Icon(Icons.image),
+              label: Text('Add image'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
